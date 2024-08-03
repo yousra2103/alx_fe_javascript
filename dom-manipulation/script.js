dc65bb3427ffa9387fd03b9c1738c62294c1a300
Yousr <youssrabenlimam88@gmail.com>
2024-08-04 00:02:53 +0100

    quoteStorage1

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -69,11 +69,32 @@ function loadQuotes() {
 
 
 
+// Function to export quotes to a JSON file
+function exportToJasonFile() {
+    const dataStr = JSON.stringify(quotes, null, 2);
+    const blob = new Blob([dataStr], { type: 'application/json' });
+    const url = URL.createObjectURL(blob);
+
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = 'quotes.json';
+    a.click();
+    URL.revokeObjectURL(url);
+}
 
 
 
 
-
+function importFromJsonFile(event) {
+    const fileReader = new FileReader();
+    fileReader.onload = function(event) {
+      const importedQuotes = JSON.parse(event.target.result);
+      quotes.push(...importedQuotes);
+      saveQuotes();
+      alert('Quotes imported successfully!');
+    };
+    fileReader.readAsText(event.target.files[0]);
+  }
 
 
 
@@ -82,6 +103,7 @@ function loadQuotes() {
 
 // Adding event listeners to buttons
 document.getElementById('showQuoteButton').addEventListener('click', showRandomQuote);
+document.getElementById('exportQuotesButton').addEventListener('click', exportToJasonFile);
 
 // Initial setup
 document.addEventListener('DOMContentLoaded', () => {
